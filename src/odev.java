560ac6b7d9820e8bacedfd8172f82c8e41a9eb9b
sbalci0 <samilbalci.us@gmail.com>
2020-05-30 17:07:01 -0400

    new static block

---
+++ src/odev.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class odev {
+
+    static {
+        int a = 1;
+        System.out.println(a);
+    }
+   // Create a 2D String Array which is
+//[{"new jersey","atlanta","ohio"} ,
+    //{"Pittsburgh" ,"ohio","new york","ohio"} ,
+   // {"ohio","new york"}]
+
+   // Change all the ohio to Florida
+
+   // print the array
+   public static void main(String[] args) {
+       String[][] citys={{"new jersey","atlanta","ohio"} ,
+       {"Pittsburgh" ,"ohio","new york","ohio"} ,
+        {"ohio","new york"}};
+
+       for (int i = 0; i <citys.length ; i++) {
+           for (int j = 0; j <citys[i].length ; j++) {
+               if(citys[i][j].contains("ohio")){
+                  citys[i][j]="florida";
+               }
+               System.out.println(citys[i][j]+" ");
+           }
+
+       }
+
+   }
+
+
+
+}
